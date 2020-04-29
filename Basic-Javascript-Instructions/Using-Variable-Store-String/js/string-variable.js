ed93675795dec6a3e48c1eaa6247a1749c2d6031
Felipe Machado <Feelipe@email.com>
2020-04-28 22:37:32 -0300

    Using Variable Store String

---
+++ Basic-Javascript-Instructions/Using-Variable-Store-String/js/string-variable.js
@@ -0,0 +1,8 @@
+let username = "Molly";
+let message = "See our upcoming range";
+
+let name = document.getElementById("name");
+name.textContent = username;
+
+let note = document.getElementById("note");
+note.textContent = message;
