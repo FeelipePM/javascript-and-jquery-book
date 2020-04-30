08ac2a437fad027ccdc50df5966930c8c549f0ba
Felipe Machado <Feelipe@email.com>
2020-04-30 12:59:30 -0300

    Using Quotes Inside String

---
+++ Basic-Javascript-Instructions/Using-Quotes-Inside-String/js/string-with-quotes.js
@@ -0,0 +1,8 @@
+let title = "Molly's Special Offers";
+let message = '<a href="sale.html"/>25% off!';
+
+let newTitle = document.getElementById("title");
+newTitle.innerHTML = title;
+
+let newMessage = document.getElementById("note");
+newMessage.innerHTML = message;
