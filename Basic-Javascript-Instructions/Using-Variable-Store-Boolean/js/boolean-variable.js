e0cfc5b28022c5c6bd89f890cdfb2bdfabaaa117
Felipe Machado <Feelipe@email.com>
2020-04-29 23:19:09 -0300

    Using Variable Store Boolean

---
+++ Basic-Javascript-Instructions/Using-Variable-Store-Boolean/js/boolean-variable.js
@@ -0,0 +1,8 @@
+let inStock = true;
+let shipping = false;
+
+let stock = document.getElementById("stock");
+stock.className = inStock;
+
+let ship = document.getElementById("shipping");
+ship.className = shipping;
