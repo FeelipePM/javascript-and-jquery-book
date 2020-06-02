37a4cd8f81426fb8e2d050511bf316b97fde93c0
Felipe Machado <Feelipe@email.com>
2020-06-01 23:42:16 -0300

    Using String Operators

---
+++ Basic-Javascript-Instructions/Using-string-operators/js/string-operator.js
@@ -0,0 +1,9 @@
+let greeting = 'Hello ';
+
+let name = 'Molly';
+
+let welcomeMessage = greeting + name + '!';
+
+let element = document.getElementById('greeting');
+
+element.textContent = welcomeMessage;
