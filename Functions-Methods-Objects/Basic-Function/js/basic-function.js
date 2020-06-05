c70ca1b61d24ff951479c1a36e1a3de9c25939ca
Felipe Machado <Feelipe@email.com>
2020-06-04 23:52:28 -0300

    Basic Function

---
+++ Functions-Methods-Objects/Basic-Function/js/basic-function.js
@@ -0,0 +1,9 @@
+let message = "Sign up to receive our newsletter for 10% off!";
+
+
+function updateMessage() {
+  let element = document.getElementById("message");
+  element.textContent = message;
+}
+
+updateMessage();
