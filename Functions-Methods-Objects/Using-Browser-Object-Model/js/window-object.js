58f85ab3ff677592619606d05cd5001f9d3ad7be
Felipe Machado <Feelipe@email.com>
2020-06-25 23:29:46 -0300

    Using the Browser Object Model

---
+++ Functions-Methods-Objects/Using-Browser-Object-Model/js/window-object.js
@@ -0,0 +1,15 @@
+let msg = '<h2>browser window</h2><p>width: ' + window.innerWidth + '</p>';
+
+msg += '<p>height: ' + window.innerHeight + '</p>';
+
+msg += '<h2>history</h2><p>items: ' + window.history.length + '</p>';
+
+msg += '<h2>screen</h2><p>width: ' + window.screen.width + '</p>';
+
+msg += '<p>height: ' + window.screen.height + '</p>';
+
+let element = document.getElementById('info');
+
+element.innerHTML = msg;
+
+alert('Current page: ' + window.location);
